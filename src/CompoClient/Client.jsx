bde0b655697042dc543181e54b8f1f5cfee0623d
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-08-03 19:57:40 +0600

    add client response added successfully

--- src/CompoClient/Client.jsx
+++ src/CompoClient/Client.jsx
@@ -6,11 +6,79 @@ const Client = () => {
 
 
     const notify = (message) => {
-        toast.success(`${message}`)
+        toast.custom((t) => (
+            <div
+                className={`${t.visible ? 'animate-enter' : 'animate-leave'
+                    } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+            >
+                <div className="flex-1 w-0 p-4">
+                    <div className="flex items-start">
+                        <div className="flex-shrink-0 pt-0.5">
+                            <img
+                                className="h-10 w-10 rounded-full"
+                                src="../../public/icons8-success.gif"
+                                alt=""
+                            />
+                        </div>
+                        <div className="ml-3 my-auto flex-1">
+                            <p className="text-sm font-medium text-gray-900">
+                                {message}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex border-l border-gray-200">
+                    <button
+                        onClick={() => toast.dismiss(t.id)}
+                        className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    >
+                        <img
+                                className="h-10 w-10 rounded-full"
+                                src="../../public/icons8-close (new).gif"
+                                alt=""
+                            />
+                    </button>
+                </div>
+            </div>
+        ))
 
     };
     const falseNotify = (message) => {
-        toast.error(`${message}`)
+        toast.custom((t) => (
+            <div
+                className={`${t.visible ? 'animate-enter' : 'animate-leave'
+                    } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+            >
+                <div className="flex-1 w-0 p-4">
+                    <div className="flex items-start">
+                        <div className="flex-shrink-0 pt-0.5">
+                            <img
+                                className="h-10 w-10 rounded-full"
+                                src="../../public/icons8-high-priority.gif"
+                                alt=""
+                            />
+                        </div>
+                        <div className="ml-3 my-auto flex-1">
+                            <p className="text-sm font-medium text-gray-900">
+                                {message}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex border-l border-gray-200">
+                    <button
+                        onClick={() => toast.dismiss(t.id)}
+                        className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    >
+                        <img
+                                className="h-10 w-10 rounded-full"
+                                src="../../public/icons8-close (new).gif"
+                                alt=""
+                            />
+                    </button>
+                </div>
+            </div>
+        ))
 
     };
 
