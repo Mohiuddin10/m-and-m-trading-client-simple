cd11a1594bfcb6b375b28211a41b0889bde4da09
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-08-14 01:33:19 +0600

    edit todos

--- z.TODOS.js
+++ z.TODOS.js
@@ -2,6 +2,9 @@
 // Create new field for comments   ==> Done
 // add footer that don't require any signature
 // find client information by name and show in final truck chalan     ===> Done
-// add fiend for date and time ====>Working .....
+// add fiend for date and time ====> Done
+
+
+// next task Work on footer ==> report ==> sorted report ==> add serial number
 
 
