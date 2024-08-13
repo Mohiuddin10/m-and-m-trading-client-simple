5fba1e6fb67f23c13aa2e4b3cb98101ba8dfb4b8
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-08-13 21:53:12 +0600

    final truck chalan part complete

--- src/TruckChalan/ShowChalan/Party.jsx
+++ src/TruckChalan/ShowChalan/Party.jsx
@@ -1,10 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 const Party = (params) => {
-    console.log(params.id);
+    const [ client, setClient] = useState({});
+    useEffect(() => {
+        console.log(`${name}`);
+        fetch(`http://localhost:3001/client/${params.clientID}`)
+        .then(res => res.json())
+        .then(data => setClient(data.data))
+    },[])
     
-   
+   console.log(client);
  
     return (
         <div className="m-5 p-6 border rounded-lg">
@@ -12,15 +18,15 @@ const Party = (params) => {
                 <tbody>
                     <tr className="">
                         <td className="border border-slate-700 px-4 py-2">Consign:    </td>
-                        <td className="border border-slate-700 px-4 py-2"></td>
+                        <td className="border border-slate-700 px-4 py-2">{client.name}</td>
                     </tr>
                     <tr>
                         <td className="border border-slate-700  px-4 py-2">Address:</td>
-                        <td className="border border-slate-700  px-4 py-2"></td>
+                        <td className="border border-slate-700  px-4 py-2">{client.address}</td>
                     </tr>
                     <tr>
                         <td className="border border-slate-700  px-4 py-2">Phone:</td>
-                        <td className="border border-slate-700  px-4 py-2">0</td>
+                        <td className="border border-slate-700  px-4 py-2">0{client.phone}</td>
                     </tr>
                 </tbody>
             </table>
