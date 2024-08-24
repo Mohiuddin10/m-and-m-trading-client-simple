b5057843684a0f52ffa332e63ec772a3c2f1c834
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-08-25 00:34:01 +0600

    delete done

--- src/TruckReport/SingleTruck.jsx
+++ src/TruckReport/SingleTruck.jsx
@@ -1,22 +1,66 @@
+import toast, { Toaster } from "react-hot-toast";
 
 const SingleTruck = (props) => {
     console.log(props.sl);
-    const { driverPhone, createdAt, itemName, truckNumber, weight, comments, fare, advance, bags} = props.singleTruck;
+    const { driverPhone, createdAt, itemName, truckNumber, weight, comments, fare, advance, bags, _id} = props.singleTruck;
     const dateObject = new Date(createdAt);
     const formattedDate = dateObject.toDateString();
 
 
+    const notify = (data) => {
+        toast.custom((t) => (
+            <div
+                className={`${t.visible ? 'animate-enter' : 'animate-leave'
+                    } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+            >
+                <div className="flex-1 w-0 p-4">
+                    <div className="flex items-start">
+                        <div className="flex-shrink-0 pt-0.5">
+                            <img
+                                className="h-10 w-10 rounded-full"
+                                src="../../public/icons8-bin.gif"
+                                alt=""
+                            />
+                        </div>
+                        <div className="ml-3 my-auto flex-1">
+                            <p className="text-sm font-medium text-gray-900">
+                                {data.message}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex border-l border-gray-200">
+                    <button
+                        onClick={() => toast.dismiss(t.id)}
+                        className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    >
+                        <img
+                                className="h-10 w-10 rounded-full"
+                                src="../../public/icons8-close.gif"
+                                alt=""
+                            />
+                    </button>
+                </div>
+            </div>
+        ))
+    }
+
+    const falseNotify = (data) => {
+        console.log(data);
+        toast.error(`${data.message}`)
+
+    };
+
+
 
     const handleDelete = (id) => {
-        fetch(`http://localhost:3001/client/${id}`, {
+        fetch(`http://localhost:3001/truck/${id}`, {
             method: "Delete"
         })
             .then(res => res.json())
             .then(data => {
                 console.log(data)
                 data.success ? notify(data) : falseNotify(data)
-                const updateClients = clients.filter((client) => client._id != id)
-                setClients(updateClients);
             })
        
 
@@ -36,7 +80,7 @@ const SingleTruck = (props) => {
                 <td>{fare - advance}</td>
                 <td>{comments}</td>
                 <td><button className="mx-auto btn btn-primary">Update</button></td>
-                <td><button onClick={() => handleDelete()} className="mx-auto btn btn-warning">Delete</button></td>
+                <td><button onClick={() => handleDelete(_id)} className="mx-auto btn btn-warning">Delete</button></td>
             </tr>
             </tbody>
     );
--- z.TODOS.js
+++ z.TODOS.js
@@ -2,10 +2,12 @@
 // Create new field for comments   ==> Done
 // add footer that don't require any signature
 // find client information by name and show in final truck chalan     ===> Done
-// add fiend for date and time ====> Done
+// add field for date and time ====> Done
 
 
 // next task Work on footer ==> report ==> sorted report ==> add serial number
 
 
-// ===> working on delete truck data 
+// ===> working on delete truck data ==> Done
+
+// add menual date in truck 
