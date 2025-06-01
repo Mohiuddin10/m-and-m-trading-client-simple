36280da214f5ba67c60576eeaa4dd8c5e72b3164
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-06-02 00:24:58 +0600

    report issue solved

--- src/TruckReport/AllReport.jsx
+++ src/TruckReport/AllReport.jsx
@@ -2,90 +2,90 @@ import { useEffect, useState } from "react";
 import SingleTruck from "./SingleTruck";
 
 const AllReport = (props) => {
-    console.log(props.selectedClient);
-    const { _id } = props.selectedClient;
-    const date = props.date;
-    console.log(date);
-    const [truckData, setTruckData] = useState([])
+  const { _id } = props.selectedClient;
+  const { setTruckData, truckData } = props;
+  const date = props.date;
+  //   const [truckData, setTruckData] = useState([]);
 
-    useEffect(() => {
-        fetch(`https://m-and-m-trading-server.onrender.com/report/${_id}`)
-            .then(res => res.json())
-            .then(data => {
-                // setTruckData(data)
-                const newSort = data.filter(singleData => {
-                    const date1 = new Date(singleData.date);
-                    const date2 = new Date(date)
-                    return (date1.toDateString() == date2.toDateString())
-                });
-                setTruckData(newSort)
+  useEffect(() => {
+    setTruckData([]);
+    fetch(`https://m-and-m-trading-server.onrender.com/report/${_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // setTruckData(data)
 
-            })
-    }, [_id]);
+        const newSort = data.filter((singleData) => {
+          const date1 = new Date(singleData.date);
+          const date2 = new Date(date);
+          return date1.toDateString() == date2.toDateString();
+        });
+        setTruckData(newSort);
+      });
+  }, [_id, date]);
 
-    const handleDelete = (id) => {
-        console.log(id);
-        fetch(`https://m-and-m-trading-server.onrender.com/truck/${id}`, {
-            method: "Delete"
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                const updateTruckData = truckData.filter(data => data._id !== id)
-                setTruckData(updateTruckData)
-            })
-    }
+  const handleDelete = (id) => {
+    console.log(id);
+    fetch(`https://m-and-m-trading-server.onrender.com/truck/${id}`, {
+      method: "Delete",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const updateTruckData = truckData.filter((data) => data._id !== id);
+        setTruckData(updateTruckData);
+      });
+  };
 
-    console.log(truckData);
-    let totalWeight = 0;
-    truckData.map(sData => totalWeight = totalWeight + sData.weight)
-    console.log(totalWeight);
-    return (
-        <div>
-            <div className="overflow-x-auto">
-                <table className="table table-xs">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Date</th>
-                            <th>Truck Number</th>
-                            <th>Phone</th>
-                            <th>Item</th>
-                            <th>Bags</th>
-                            <th>Weight</th>
-                            <th>Fare</th>
-                            <th>Advance</th>
-                            <th>Net Pay</th>
-                            <th>Comments</th>
-                        </tr>
-                    </thead>
+  let totalWeight = 0;
+  truckData.map((sData) => (totalWeight = totalWeight + sData.weight));
 
-                    {
-                        truckData.map(singleTruck => <SingleTruck key={_id}
-                            sl={truckData.indexOf(singleTruck) + 1}
-                            singleTruck={singleTruck}
-                            handleDelete={handleDelete}></SingleTruck>)
-                    }
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th className="text-xl font-bold">Total</th>
-                            <th></th>
-                            <th className="text-xl font-bold">{totalWeight}</th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table table-xs">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Date</th>
+              <th>Truck Number</th>
+              <th>Phone</th>
+              <th>Item</th>
+              <th>Bags</th>
+              <th>Weight</th>
+              <th>Fare</th>
+              <th>Advance</th>
+              <th>Net Pay</th>
+              <th>Comments</th>
+            </tr>
+          </thead>
 
-        </div>
-    );
+          {truckData.map((singleTruck) => (
+            <SingleTruck
+              key={_id}
+              sl={truckData.indexOf(singleTruck) + 1}
+              singleTruck={singleTruck}
+              handleDelete={handleDelete}
+            ></SingleTruck>
+          ))}
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th className="text-xl font-bold">Total</th>
+              <th></th>
+              <th className="text-xl font-bold">{totalWeight}</th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllReport;
--- src/TruckReport/TruckReport.jsx
+++ src/TruckReport/TruckReport.jsx
@@ -2,46 +2,69 @@ import { useEffect, useState } from "react";
 import AllReport from "./AllReport";
 
 const TruckReport = () => {
-    const [clients, setClients] = useState([])
-    const [selectedClient, setSelectedClient] = useState([])
-    const [selectedDate, setSelectedDate] = useState(null);
-    useEffect(() => {
-        fetch("https://m-and-m-trading-server.onrender.com/client")
-            .then(res => res.json())
-            .then(data => setClients(data.data))
-    }, [])
+  const [clients, setClients] = useState([]);
+  const [selectedClient, setSelectedClient] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [truckData, setTruckData] = useState([]);
+  useEffect(() => {
+    fetch("https://m-and-m-trading-server.onrender.com/client")
+      .then((res) => res.json())
+      .then((data) => setClients(data.data));
+  }, []);
 
-    const handleClientSearch = (e) => {
-        e.preventDefault();
-        const clientName = e.target.clientName.value;
-        const selectedClient = clients.find(client => client.name == clientName);
-        setSelectedClient(selectedClient)
+  const handleClientSearch = (e) => {
+    e.preventDefault();
+    const clientName = e.target.clientName.value;
+    const selectedClient = clients.find((client) => client.name == clientName);
+    setSelectedClient(selectedClient);
 
-        const date = e.target.date.value;
-        const d = new Date(date);
-        const formattedDate = d.toDateString();
-        setSelectedDate(formattedDate);
-    }
+    const date = e.target.date.value;
+    const d = new Date(date);
+    const formattedDate = d.toDateString();
+    setSelectedDate(formattedDate);
+  };
 
-
-    return (
-        <div className="">
-            <form className="text-center sm:flex-col md:flex-row align-middle justify-center items-center my-4" action="" onSubmit={handleClientSearch}>
-                <select className="select m-2 select-bordered w-full max-w-xs" name="clientName" >
-                    <option disabled selected>Client name</option>
-                    {
-                        clients.map(client => <option key={client._id}>{client?.name}</option>)
-                    }
-                </select>
-                <input className="rounded-lg p-2 m-2" type="date" name="date" id="date" />
-                <button type="submit" className="btn btn-wide m-2">Done</button>
-            </form>
-            <div className="">
-                <AllReport selectedClient={selectedClient} date={selectedDate}></AllReport>
-            </div>
-            <h2 className="text-center m-8">This is auto generated report and doesn't require any signature</h2>
-        </div>
-    );
+  return (
+    <div className="">
+      <form
+        className="text-center sm:flex-col md:flex-row align-middle justify-center items-center my-4"
+        action=""
+        onSubmit={handleClientSearch}
+      >
+        <select
+          className="select m-2 select-bordered w-full max-w-xs"
+          name="clientName"
+        >
+          <option disabled selected>
+            Client name
+          </option>
+          {clients.map((client) => (
+            <option key={client._id}>{client?.name}</option>
+          ))}
+        </select>
+        <input
+          className="rounded-lg p-2 m-2"
+          type="date"
+          name="date"
+          id="date"
+        />
+        <button type="submit" className="btn btn-wide m-2">
+          Done
+        </button>
+      </form>
+      <div className="">
+        <AllReport
+          truckData={truckData}
+          setTruckData={setTruckData}
+          selectedClient={selectedClient}
+          date={selectedDate}
+        ></AllReport>
+      </div>
+      <h2 className="text-center m-8">
+        This is auto generated report and doesn't require any signature
+      </h2>
+    </div>
+  );
 };
 
 export default TruckReport;
